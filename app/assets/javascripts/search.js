fccc0cce2ed1da10751067526b81c6f1b3b94a1f
Prabhav Jain <prabhav@Prabhavs-MacBook-Pro.local>
2013-05-18 00:17:03 -0400

    added json conversion for search results as well as callback

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,35 @@
 
 
-    function search(term){
+    function search(name, loc, callback){
         $.ajax({
             type: "GET",
             url: "http://www.zvents.com/partner_rest/search",
             crossDomain: true,
             data: { 
                 key: "TEKACENXPPGFKKYYCOIKWIUALTPNABMORQSXJBWOMNNEVDLQAFNUMJTQRIJWBCRX", 
-                where: term 
+                where: loc,
+                what: name
             },
-            success: function(data){
-                console.log(data)
+            dataType: "xml",
+            success: function(xml){
+                var events = []
+                $(xml).find('event').each(function(){
+                   var id = $(this).attr('id')
+                   var name = $(this).find('name').text();
+
+                   var images = []
+
+                   $(this).find('images').each(function(){
+                    images.push($(this).find("url").text())
+                   });
+
+                   events.push({"id": id, "name" : name, "images":images})
+
+                });
+                console.log(events)
+                callback(events)
             }
         });
- 
-
 
     }
 
