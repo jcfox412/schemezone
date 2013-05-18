ad8b1abfa8f9cca5598e73e2bae58f63b0184371
James Fox <jcfox412@gmail.com>
2013-05-18 14:03:21 -0400

    Add prize to contest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518170933) do
+ActiveRecord::Schema.define(:version => 20130518174516) do
 
   create_table "contests", :force => true do |t|
     t.string   "image_url"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(:version => 20130518170933) do
     t.string   "title"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.string   "prize_description"
   end
 
   create_table "events", :force => true do |t|
