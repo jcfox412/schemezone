7ed7f90badee6c1a5ff71b41acc7cd021acd60e2
James Fox <jcfox412@gmail.com>
2013-05-17 20:26:37 -0400

    Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Screamzone::Application.config.secret_token = '61c0e2609d5ed65074bedecc1648281de424178ef6b587ad14294b026a9f80adb240a557d96ddbd421e01e504656e39b414ef66a759d4dd3f286ddd4ac34bf06'
