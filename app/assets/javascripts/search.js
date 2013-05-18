c76111f98035b127e5b4b2ee9eef82b26e95eb56
Prabhav Jain <prabhav@Prabhavs-MacBook-Pro.local>
2013-05-17 23:15:43 -0400

    added search.js

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function()){
+
+    $(document).ajaxSend(function(e, xhr, options) {
+      var token = $("meta[name='csrf-token']").attr("content");
+      xhr.setRequestHeader("X-CSRF-Token", token);
+    });
+    function search(term){
+        $.ajax({
+            type: "GET",
+            url: "http://www.zvents.com/partner_rest/search",
+            crossDomain: true,
+            data: { 
+                key: "TEKACENXPPGFKKYYCOIKWIUALTPNABMORQSXJBWOMNNEVDLQAFNUMJTQRIJWBCRX", 
+                where: term 
+            },
+            success: function(data){
+                console.log(data)
+            }
+        });
+    }
+}
+
+
