bf70f5d2172ae06ec99907301490260b1c3cce8d
James Fox <jcfox412@gmail.com>
2013-05-18 05:04:12 -0400

    marco committing user bar...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :current_user
 
   # returns the user currently logged in to the shopping cart.
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		puts @current_user	
 	end
 	helper_method :current_user
 	
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,15 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:email])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to root_url, :notice => "Logged in"
+			redirect_to request.referer  
 		else
 			flash.now.alert = "Invalid email or password"
-			redirect_to root_url, :notice => "Invalid email or password"
+			redirect_to request.referer, :notice => "Invalid email or password"
 		end
 	end
 
 	def destroy
 		session[:user_id] = nil
-		redirect_to root_url :notice=> "Logged out"
+		redirect_to request.referer, :notice=> "Logged out"
 	end
 end
