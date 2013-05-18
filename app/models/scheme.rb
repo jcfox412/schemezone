9037eccf92ba098495f800027dea742732a95186
Prabhav Jain <prabhav@Prabhavs-MacBook-Pro.local>
2013-05-18 03:26:50 -0400

    migration

--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -1,5 +1,5 @@
 class Scheme < ActiveRecord::Base
-  attr_accessible :creator_id, :description, :string, :title, :type, :team_name
+  attr_accessible :creator_id, :description, :string, :title, :type, :team_name, :image_url
 
   belongs_to :event
   has_and_belongs_to_many :users
